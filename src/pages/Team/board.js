9a365b8f4032008006e85676d4d4f5101085b7fd
ritik-sa0201 <ritik.sainicoding@gmail.com>
2024-12-06 12:14:23 +0530

    Update board.js
    new team update

--- src/pages/Team/board.js
+++ src/pages/Team/board.js
@@ -1,57 +1,57 @@
 export const board = [
   {
-    name: "Md. Ehtisham",
+    name: "Nikhil Singh Rajpoot",
     position: "Lead",
     image: "/assets/Ehtisham.jpeg"
   },
   {
-    name: "Nikhil Rajput",
+    name: "Nayan Kumar Sahu",
     position: "Co-Lead",
     image: "/assets/Nikhil.jpg"
   },
   {
-    name: "Rajesh Kumar Jena",
+    name: "Ritik Saini",
     position: "Technical head",
     image: "/assets/Rajesh.jpg"
   },
-  // {
-  //   name: "Brijendra Singh",
-  //   position: "Technical Co-Lead",
-  //   image: "/assets/Brijendra.jpg"
-  // },
   {
-    name: "Anirudh Pradhan",
-    position: "Events head",
-    image: "/assets/Anirudh.jpg"
-  },
+     name: "Shuvayu Chatterjee",
+     position: "Technical Co-Lead",
+     image: "/assets/Brijendra.jpg"
+   },
   {
-    name: "Partha Sarathi Mohanty",
-    position: "Events Co-lead",
-    image: "/assets/Partha.jpg"
-  },
+   // name: "Anirudh Pradhan",
+   // position: "Events head",
+   // image: "/assets/Anirudh.jpg"
+  //},
   {
-    name: "Shikha Singh",
+//    name: "Partha Sarathi Mohanty",
+  //  position: "Events Co-lead",
+    //image: "/assets/Partha.jpg"
+  //},
+  {
+    name: "Rojalin Biswal",
     position: "Marketing head",
     image: "/assets/ShikhaSingh.jpg"
   },
   {
-    name: "Bedasnata Das",
+    name: "Priyanshu Nayak",
     position: "Marketing Co-lead",
     image: "/assets/Bedasnata.jpg"
   },
   {
-    name: "Arushi Mahapatra",
+    name: "G Vaneesa Reddy",
     position: "Design head",
     image: "/assets/Arushi.jpeg"
   },
+  //{
+  //  name: "Hitasha Padhi",
+   // position: "Social Media head",
+   // image: "/assets/Hitasha.jpg"
+  //},
   {
-    name: "Hitasha Padhi",
-    position: "Social Media head",
-    image: "/assets/Hitasha.jpg"
-  },
-  {
-    name: "Priya Dash",
-    position: "Social Media & Content Co-lead",
+    name: "Yash",
+   position: "Social Media & Event Management",
     image: "/assets/Priya.jpg"
   },
 ];
